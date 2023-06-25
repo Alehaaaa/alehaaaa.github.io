1a0b07769f6097a1d398cb0eed6ad388a6bf4fa7
Alehaaaa <alehamartinva@gmail.com>
2023-06-25 12:17:42 +0200

    Logo, smooth scroll, reel navbar...

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
+var i = document.getElementById("video-volume");
+var elem = document.getElementById("playing-video");
 
 function mute_video() {
-    var i = document.getElementById("video-volume");
-    var v = document.getElementById("playing-video");
     if (i.className == "fas fa-volume-mute") {
-      i.className = "fas fa-volume-up";
-      v.muted = false;
+        i.className = "fas fa-volume-up";
+        elem.muted = false;
     } else {
-      i.className = "fas fa-volume-mute";
-      v.muted = true;
+        i.className = "fas fa-volume-mute";
+        elem.muted = true;
     }
-  }
-  
-  function fullscreen_video() {
-    var i = document.getElementById("video-volume");
-    var elem = document.getElementById("playing-video");
+}
+
+function fullscreen_video() {
     if (elem.requestFullscreen) {
         elem.requestFullscreen();
-      } else if (elem.webkitRequestFullscreen) { /* Safari */
+    } else if (elem.webkitRequestFullscreen) {
+        /* Safari */
         elem.webkitRequestFullscreen();
-      } else if (elem.msRequestFullscreen) { /* IE11 */
+    } else if (elem.msRequestFullscreen) {
+        /* IE11 */
         elem.msRequestFullscreen();
-      }
-      elem.muted = false;
-      i.className = "fas fa-volume-up";
+    }
+    elem.muted = false;
+    i.className = "fas fa-volume-up";
 }
-
 
 ham_open = () => {
-    const hamburger = document.querySelector("#hamburger")
+    const hamburger = document.querySelector("#hamburger");
     const navbar = document.querySelector("#navbar");
 
-    hamburger.classList.toggle('fa-bars');
-    hamburger.classList.toggle('fa-times');
-    navbar.classList.toggle('nav-toggle');
+    hamburger.classList.toggle("fa-bars");
+    hamburger.classList.toggle("fa-times");
+    navbar.classList.toggle("nav-toggle");
+};
 
-}
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
