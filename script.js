026cbb36352bc9ce11ce1ce0f5c0dff423920fe0
Alehaaaa <alehamartinva@gmail.com>
2023-06-25 11:45:55 +0200

    Fullscreen video + better hamburger

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 
-function change() {
+function mute_video() {
     var i = document.getElementById("video-volume");
     var v = document.getElementById("playing-video");
     if (i.className == "fas fa-volume-mute") {
-        i.className = "fas fa-volume-up";
-        v.muted = false;
+      i.className = "fas fa-volume-up";
+      v.muted = false;
     } else {
-        i.className = "fas fa-volume-mute";
-        v.muted = true;
+      i.className = "fas fa-volume-mute";
+      v.muted = true;
     }
+  }
+  
+  function fullscreen_video() {
+    var i = document.getElementById("video-volume");
+    var elem = document.getElementById("playing-video");
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+      } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+      }
+      elem.muted = false;
+      i.className = "fas fa-volume-up";
 }
 
 
-clicked = () => {
+ham_open = () => {
     const hamburger = document.querySelector("#hamburger")
     const navbar = document.querySelector("#navbar");
 
